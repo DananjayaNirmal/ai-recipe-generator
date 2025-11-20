adf449a00fc5758ec1bcfd83aa8a83211ed201d8
DananjayaNirmal <dananjayanirmal.h@gmail.com>
2025-11-20 22:27:38 +0530

    -

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+/*import {createRoot} from "react-dom/client"
+import Header from "./componants/Header"
+import Main from "./componants/Main"
+//import App from "./App"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <>
+    <Header/>
+    <Main/>
+    </>
+)*/
